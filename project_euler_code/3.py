22917968f4ca675f1d9c5ceb4ddf7c42d93c0a57
1995eaton <1995eaton@gmail.com>
2013-09-12 01:26:45 +0000

    updated

--- project_euler_code/3.py
+++ project_euler_code/3.py
@@ -1,37 +1,16 @@
 #!/usr/bin/python
 
-import sys
+#ptest checks a number's factors for primes - does not use seive's method
 
-list1 = []
-num = 600851475143
+def ptest(num):
+	for a in range(1, num + 1):
+		if num % a == 0:
+			count = 0
+			for p in range(1,a + 1):
+				if a % p == 0:
+					count+= 1
+			if count == 2:
+				print(a, "is prime.")
 
-#def main():
-	#question = input("How many digits?")
-	#for x in range(int(question)):
-		#count = 0
-		#for y in range(1,x):
-			#if x % y == 0:
-				#count += 1
-		#if count < 2:
-			#list1.append(x)
-			##print(x)
-	##print(list1)
-	#for g in list1[1:len(list1)]:
-		#if num % g == 0:
-			#print(g)
-
-def main():
-	question = input("Number:")
-	while question.isdigit() == False:
-		question = input("Number:")
-	count = 0
-	for y in range(1,int(question)):
-		if int(question) % y == 0:
-			for q in range(1,int(question)):
-				count = 0
-				if y % q == 0:
-					count += 1
-				print(count)
-				if count < 2:
-					print(q)
-main()
+i = input("Enter a number: ")
+ptest(int(i))
--- project_euler_code/5.py
+++ project_euler_code/5.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
-l1 = []
+
 def main():
+
 	n = 1
 	while True:
 		count = 0
 		q = 2520 * n
 		num = [11,13,15,16,17,19]
-		for s in range(1, len(num)):
+		for s in range(len(num)):
 			if q % num[s] == 0:
 				count += 1
-				if count == 5:
+				if count == 6:
 					print(q)
-					#return True
+					return True
 		n = n + 1
+
 main()

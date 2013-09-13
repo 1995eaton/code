e0ad6a9a8b94710c1ba2314b77d5de4790f7ede8
1995eaton <1995eaton@gmail.com>
2013-09-13 22:45:17 +0000

    upd

--- project_euler_code/36.py
+++ project_euler_code/36.py
@@ -1,75 +1,27 @@
 #!/usr/bin/python
-def palcheck(isrange, x):
-	lpal = []
-	if isrange == False:
-		x = str(x)
-		lenx = 0
-		lenxx = len(x) - 1
-		if len(x) % 2 == 0:
-			while lenx < len(x) / 2:
-				if x[lenx] == x[lenxx]:
-					lenx += 1
-					lenxx -= 1
-					if lenx == (len(x) / 2):
-						return True
-				else:
-					return False
-					break
-		if len(x) % 2 != 0:
-			while lenx <= lenxx:
-				if x[lenx] == x[lenxx]:
-					lenx += 1
-					lenxx -= 1
-					if lenx == (int(len(x) / 2) + 1):
-						return True
-				else:
-					return False
-					break
-		return lpal
-	if isrange == True:
-		for x in range(x):
-			x = str(x)
-			lenx = 0
-			lenxx = len(x) - 1
-			if len(x) % 2 == 0:
-				while lenx < len(x) / 2:
-					if x[lenx] == x[lenxx]:
-						lenx += 1
-						lenxx -= 1
-						if lenx == len(x) / 2:
-							lpal.append(x)
-					else:
-						lpal.append(False)
-						break
-			if len(x) % 2 != 0:
-				while lenx <= lenxx:
-					if x[lenx] == x[lenxx]:
-						lenx += 1
-						lenxx -= 1
-						if lenx == (int(len(x) / 2) + 1):
-							lpal.append(x)
-					else:
-						lpal.append(False)
-						break
-		return lpal
+import time
+def palcheck(x):
+	x = str(x)
+	if len(x) % 2 == 0:
+		if x == x[::-1]:
+			return True
+	else:
+		if x == x[::-1]:
+			return True
+	return False
+def b10to2(y):
+	b2 = str("")
+	t = 0
+	while 2**t < y:
+		t += 1
+	for x in range(t + 1,-1,-1):
+		if 2**x <= y:
+			b2 += "1"
+			y -= 2**x
+		else:
+			b2 += "0"
+	return int(b2)
 
-def base2_convert(xx):
-	sumyorig = 0
-	for y in xx:
-	#for y in (palcheck(True, 1000000)):
-		l1 = []
-		y = int(y)
-		yorig = y
-		for x in range(22,-1,-1):
-			z = 2**x
-			if z <= y:
-				l1.append("1")
-				y = y - 2**x
-			else:
-				if len(l1) != 0:
-					l1.append("0")
-		if palcheck(False, "".join(l1)) == True:
-			print(yorig, "=", "".join(l1))
-			sumyorig += yorig
-	return sumyorig
-print(base2_convert(palcheck(True, 1000000)))
+start_time = time.time()
+print(sum([x for x in range(1000000) if palcheck(x) == True and palcheck(b10to2(x)) == True]))
+print("Completed in", time.time() - start_time, "seconds")

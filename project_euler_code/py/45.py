a6901d6f447e1509b1f768e748e7baf08698bee3
1995eaton <1995eaton@gmail.com>
2013-10-06 02:20:45 +0000

    45 added

---
+++ project_euler_code/py/45.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+limit = 300000
+
+l1 = set([])
+l2 = set([])
+
+for x in range(2, limit + 1):
+	l1.add(int(x * (x + 1) / 2))
+for x in range(2, limit + 1):
+	pent = int(x * (3 * x - 1) / 2)
+	if pent in l1:
+		l2.add(pent)
+for x in range(2, limit + 1):
+	hexa = int(x * (2 * x - 1))
+	if hexa in l2:
+		print(hexa)

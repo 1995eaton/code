e8322fa2750e563ca52637d362eeeda438bed960
Jake Eaton <jake@jakes-mbp.gateway.pace.com>
2013-11-08 14:55:19 -0500

    updated

--- word_unscramble/unscrambler.py
+++ word_unscramble/unscrambler.py
@@ -1,44 +1,27 @@
-#!/usr/bin/python
+import sys, re, os
 
-from collections import Counter; import sys; import re; import random; import time
+def Unscramble(words, isFile):
+	if os.path.isfile("dictionary"):
+		dictionary = (open("dictionary", "r").read()).rsplit()
+		if isFile and os.path.isfile(words):
+			input_file = [i for i in re.sub("\W", " ", (open(words, "r").read()).lower()).split(" ") if len(i) != 0]
+			for i0 in input_file:
+				for i1 in dictionary:
+					if len(i0) == len(i1) and sorted(i0) == sorted(i1):
+						print(i1)
+		else:
+			for i0 in words:
+				for i1 in dictionary:
+					if len(i0) == len(i1) and i0 == sorted(i1):
+						print(i1)
+	else:
+		print("Dictionary does not exist")
 
 def main():
-	if sys.argv[1] == "-S":
-		for x in sys.argv[2:]:
-			lx = list(x)
-			while True:
-				random.shuffle(lx)
-				print("".join(lx))
-				time.sleep(.25)
-	elif sys.argv[1] == "-s":
-		for x in sys.argv[2:]:
-			lx = list(x)
-			random.shuffle(lx)
-			print("".join(lx))
+	if sys.argv[1] == "-f":
+		Unscramble(str(sys.argv[2]), 1)
 	else:
-		try:
+		Unscramble([sorted(str(i)) for i in sys.argv], 0)
 
-			do = open("dictionary", "r")
-			try:
-
-				infile_open = open(sys.argv[1], "r")
-				infile_read = infile_open.read()
-				infile_clear = [x for x in re.sub("\W", " ", infile_read.lower()).split(" ") if len(x) != 0]
-				for x in str.split(do.read()):
-					for y in infile_clear:
-						if len(x) == len(y) and Counter(x) == Counter(y):
-							print(x)
-
-			except FileNotFoundError:
-
-				for x in str.split(do.read()):
-					for y in sys.argv:
-						if len(x) == len(y) and Counter(x) == Counter(y):
-							print(x)
-
-		except FileNotFoundError: print("Error: dictionary file not found")
-
-try:
+if __name__ == "__main__":
 	main()
-except (IndexError, KeyboardInterrupt):
-	pass

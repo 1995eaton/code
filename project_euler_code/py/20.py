677aef592fcbbaefe57dff8b29677807691c9b60
1995eaton <1995eaton@gmail.com>
2013-11-08 20:39:55 -0500

    refresh

--- project_euler_code/py/20.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-import math
-print(sum([ int(a) for a in str(math.factorial(100))]))
--- project_euler_code/py/42.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import string, os, re; from collections import Counter
-nzc = [[x[0], x[1]] for x in zip(Counter([sum([((string.ascii_lowercase).index(i)+1) for i in str(x)]) for x in re.findall(r"\b(\w+)\b", (open("words.txt", "r")).read().lower())]).values(), set(Counter([sum([((string.ascii_lowercase).index(i)+1) for i in str(x)]) for x in re.findall(r"\b(\w+)\b", (open("words.txt", "r")).read().lower())]).elements()))]
-print(sum([x[0] for x in nzc if x[1] in [int((n/2)*(n+1)) for n in range(1, max(nzc[len(nzc)-1])) if (n/2)*(n+1) < max(nzc[len(nzc)-1])]]))
-print(sum([x[0] for x in nzc if x[1] in [int((n/2)*(n+1)) for n in range(1, max(nzc[len(nzc)-1])) if (n/2)*(n+1) < max(nzc[len(nzc)-1])]]))

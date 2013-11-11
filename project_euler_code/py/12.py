d2046d69056cecce7853e3c2ee3fb3b93a5d6ef8
1995eaton <1995eaton@gmail.com>
2013-11-11 17:53:02 -0500

    12.py

---
+++ project_euler_code/py/12.py
@@ -0,0 +1,18 @@
+def Tri(lim):
+	s = 0
+	for i in range(1, lim + 1):
+		s += i
+		yield s
+
+def main():
+	for i in list(Tri(50000)):
+		c = 0
+		if not i % 2:
+			c = 1
+		for i2 in range(1, int(i**0.5)):
+			if not i % i2:
+				c += 2
+			if c >= 500:
+				return i
+
+print(main())

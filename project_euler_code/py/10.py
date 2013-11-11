2fcaaee981e1ca5cd695a84bbd35884e84b9b897
1995eaton <1995eaton@gmail.com>
2013-11-11 17:41:25 -0500

    10.py

--- project_euler_code/py/10.py
+++ project_euler_code/py/10.py
@@ -1,32 +1,9 @@
-#!/usr/bin/python
+import numpy
 
-import math
+def main(lim):
+ sieve = numpy.ones((lim, ), dtype = numpy.bool)
+ for i in range(2, int(lim**0.5)):
+  sieve[i**2::i] = False
+ return sum([index for index, i in enumerate(sieve[2:]) if i == True and index < 2000000])
 
-def simple_prime(num):
-	z = 0
-	l1 = []
-	for n in range(2,round(num)):
-		c = 0
-		for n2 in range(2,round(num)):
-			if n % n2 == 0:
-				if n != n2:
-					c += 1
-		if c == 0:
-			l1.append(n)
-	return l1
-
-def main():
-	z = 0
-	primes = simple_prime(math.sqrt(1000000))
-	for n in range(2,1000000):
-		c = 0
-		for n2 in primes[:len(primes)]:
-			if n % n2 == 0:
-				if n != n2:
-					c += 1
-		if c == 0:
-			print(n)
-			z += n
-	#print(z)
-
-main()
+print(main(2100000))

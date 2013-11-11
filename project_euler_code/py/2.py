dc680d9444582019ae21e37f419255243c8c1ecb
1995eaton <1995eaton@gmail.com>
2013-11-11 16:45:21 -0500

    2.py update

--- project_euler_code/py/2.py
+++ project_euler_code/py/2.py
@@ -1,16 +1,7 @@
-#!/usr/bin/python
+def Fib(lim):
+ a, b = 0, 1
+ while a < lim:
+  yield a
+  a, b = b, a + b
 
-import sys
-
-q = 0
-x = 1
-z = 0
-
-while x < 4000000:
-	if q % 2 != 0:
-		z += q
-	q += x
-	x += q
-	if (x - q) % 2 != 0:
-		z += (x - q)
-	print(z)
+print(sum([i for i in list(Fib(4000000)) if not i % 2]))

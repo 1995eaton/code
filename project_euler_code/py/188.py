3f2af7fa277848ff2a1310665d7b691e8e245b78
1995eaton <1995eaton@gmail.com>
2013-11-12 16:41:00 -0500

    188.py

---
+++ project_euler_code/py/188.py
@@ -0,0 +1,12 @@
+def Mexp(b, e, m, r = 1):
+	while e > 0:
+		if e % 2 == 1:
+			r = (r * b) % m
+		e, b = e >> 1, b**2 % m
+	return r
+def main(n = 1855):
+	for i in range(1855, 0, -1):
+		n = Mexp(1777, n, 10**8)
+	return n
+
+print(main())

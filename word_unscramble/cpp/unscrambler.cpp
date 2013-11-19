ed0557c3f2ddfeab8ac7b66ad7e6f8bb0de26030
1995eaton <1995eaton@gmail.com>
2013-11-19 13:41:05 -0500

    cpp

---
+++ word_unscramble/cpp/unscrambler.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	for(int index = 1; index < argc; ++index) {
+
+		string line;
+		string word_in = argv[index];
+		cout << word_in << ":\n";
+		ifstream dictionary("dictionary.txt");
+
+		while(getline(dictionary, line)) {
+
+			if(line.length() == word_in.length()) {
+
+				string line_sorted = line; sort(line_sorted.begin(), line_sorted.end());
+				string word_in_sorted = word_in; sort(word_in_sorted.begin(), word_in_sorted.end());
+
+				if(line_sorted == word_in_sorted) {
+					cout << " " << line << endl;
+				}
+			}
+		}
+	}
+
+	return 0;
+}

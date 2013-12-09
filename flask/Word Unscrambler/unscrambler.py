da218b09ed02e7241a57bd8fd3f721aea71e92db
root <root@raspberry.gateway.pace.com>
2013-12-09 13:49:00 -0700

    flask update

--- flask/Word Unscrambler/unscrambler.py
+++ flask/Word Unscrambler/unscrambler.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def unscrambleWords():
-	if request.method == 'GET':
-		return render_template('index.html')
-	if request.method == 'POST':
-		word = request.form['WORD']
-		try:
-			dictionary = (open(str("dictionary_segments/" + str(len(word))), "r").read()).rsplit()
-		except FileNotFoundError as DNE:
-			return render_template('index.html', word = "No words of this length exist in the English language")
-		word_sorted = sorted(word)
-		words_unscrambled = []
-		for i in dictionary:
-			if len(word) == len(i) and word_sorted == sorted(i):
-				words_unscrambled.append(i)
-		return render_template('index.html', word = ' '.join(words_unscrambled))
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        word = request.form['WORD']
+        try:
+            dictionary = (open(str("dictionary_segments/" + str(len(word))), "r").read()).rsplit()
+        except FileNotFoundError as DNE:
+            return render_template('index.html', word = "No words of this length exist in the English language")
+        word_sorted = sorted(word)
+        words_unscrambled = []
+        for i in dictionary:
+            if len(word) == len(i) and word_sorted == sorted(i):
+                words_unscrambled.append(i)
+        return render_template('index.html', word = ' '.join(words_unscrambled))
 
 if __name__ == "__main__":
-	app.run()
+    app.run()

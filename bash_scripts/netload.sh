dbf67c6d5a7464095211672546c8ddb300fc677f
1995eaton <1995eaton@gmail.com>
2014-01-03 01:38:21 -0500

    Update netload.sh

--- bash_scripts/netload.sh
+++ bash_scripts/netload.sh
@@ -2,23 +2,28 @@
 
 _wldev="$(iw dev | grep -Eo "face .*" | cut --bytes=6-)"
 
-if [[ ! -d "/etc/netctl" || ! $(compgen -c | grep -E "^iw$") ]]; then
+if [ ! -d "/etc/netctl" || ! `compgen -c | grep -E "^iw$"` ]; then
 	echo "Are you sure you have netctl and/or iw installed?"
 	exit
 fi
-_pfiles="$(ls /etc/netctl | grep $_wldev)"
+
+_pfiles=`ls /etc/netctl | grep $_wldev)`
 c=1
 echo
+
 for i in $(ls /etc/netctl | grep $_wldev); do
 	echo $c $i
 	_parray[c]=$i
 	c="$(expr $c + 1)"
 done
+
 echo
 read -p "Choose a network profile: " ans
+
 if [[ $(echo $ans | grep -E "^[0-9]{,}$") -eq "" ]]; then
 	exit
 fi
+
 if [[ $ans -lt c ]]; then
 	sudo ip link set $_wldev down
 	sudo netctl stop-all

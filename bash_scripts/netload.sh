5d86d07f39134e7f7004c76afa76cc4159da5fa0
1995eaton <1995eaton@gmail.com>
2014-01-03 01:40:25 -0500

    Update netload.sh

--- bash_scripts/netload.sh
+++ bash_scripts/netload.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-_wldev=`iw dev | grep -Eo "face .*" | cut --bytes=6-)`
+_wldev=`iw dev | grep -Eo "face .*" | cut --bytes=6-`
 
 if [[ ! -d "/etc/netctl" || ! `compgen -c | grep -E "^iw$"` ]]; then
 	echo "Are you sure you have netctl and/or iw installed?"
 	exit
 fi
 
-_pfiles=`ls /etc/netctl | grep $_wldev)`
+_pfiles=`ls /etc/netctl | grep $_wldev`
 c=1
 echo
 
-for i in $(ls /etc/netctl | grep $_wldev); do
+for i in `ls /etc/netctl | grep $_wldev`; do
 	echo $c $i
 	_parray[c]=$i
 	c="$(expr $c + 1)"
@@ -20,12 +20,12 @@ done
 echo
 read -p "Choose a network profile: " ans
 
-if [[ $(echo $ans | grep -E "^[0-9]{,}$") -eq "" ]]; then
+if [[ `echo $ans | grep -E "^[0-9]{,}$"` -eq "" ]]; then
 	exit
 fi
 
 if [[ $ans -lt c ]]; then
 	sudo ip link set $_wldev down
 	sudo netctl stop-all
-	sudo netctl start ${_parray[$(echo $ans | bc)]}
+	sudo netctl start ${_parray[`echo $ans | bc`]}
 fi
